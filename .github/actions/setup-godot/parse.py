c3d2572fce0eca9f2bce15fe9340cfa2ac897e3e
bendn <bend.n@outlook.com>
2022-12-06 06:25:30 +0700

    who needs pip
    should close #6, as #6 cant happen if there is no pip, even if i cant repro

--- .github/actions/setup-godot/parse.py
+++ .github/actions/setup-godot/parse.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from packaging.version import parse
 from sys import argv
 
-parsed = parse(argv[1])
-release = argv[1].replace(
-    parsed.base_version, '').strip('.')
-print(f"{parsed.base_version}.{release if release else 'stable'}")
+parsed = argv[1].split(".")
+release = "stable"
+if len(parsed[-1]) > 1:
+    release = parsed.pop(-1)
+print(f"{'.'.join(parsed)}.{release if release else 'stable'}")
