a000243a95a200d3f606ed0424ff745b082562ca
bendn <bend.n@outlook.com>
2022-12-06 06:27:13 +0700

    update bug template

--- .github/actions/setup-godot/parse.py
+++ .github/actions/setup-godot/parse.py
@@ -6,4 +6,4 @@ parsed = argv[1].split(".")
 release = "stable"
 if len(parsed[-1]) > 1:
     release = parsed.pop(-1)
-print(f"{'.'.join(parsed)}.{release if release else 'stable'}")
+print(f"{'.'.join(parsed)}.{release}")
